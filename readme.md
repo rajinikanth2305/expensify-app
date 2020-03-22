45b5d329f3d388f8909efc2e7f57a7ae0b83e11e
rajinikanth2305 <rajinikanthreddyvarala@gmail.com>
2020-03-22 11:28:18 +0530

    production webpack build

---
+++ readme.md
@@ -0,0 +1,10 @@
+#git commands
+
+
+git init --create a new git repository
+git status  -- view the changes to your project code
+git add -add files to staging area
+git commit -creates a new commit with files from staging area
+git log--view the log status
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,13 @@
 const path = require('path');
+const ExtractTextPlugin=require('extract-text-webpack-plugin')
 
-module.exports = {
-  entry: './src/app.js',
-  output: {
+//we are using node so we dont have access to import so thats why we are using require
+module.exports =(env)=> {
+  const isProduction= env==="production";
+  const CSSExtract=new ExtractTextPlugin('styles.css');
+  return{
+    entry: './src/app.js',
+    output: {
     path: path.join(__dirname, 'public'),
     filename: 'bundle.js'
   },
@@ -14,14 +19,32 @@ module.exports = {
     },
     {
       test:/\.s?css$/,
-      use:[
-        "style-loader",
-        "css-loader",
-        "sass-loader"
-      ]
+      use:CSSExtract.extract(
+        {
+          use:[
+            {
+              loader:'css-loader',
+              options:{
+                sourceMap:true
+              }
+            },
+            {
+              loader:"sass-loader",
+              options:
+              {
+                sourceMap:true
+              }
+            }
+            
+          ]
+        }
+      )
     }]
   },
-  devtool: 'cheap-module-eval-source-map',
+  plugins:[
+    CSSExtract
+  ],
+  devtool: isProduction ?" source-map":'inline-source-map',
   devServer:{
     contentBase:path.join(__dirname, 'public'),
     port: 8080,
@@ -31,8 +54,16 @@ module.exports = {
   node: {
     fs: 'empty'
   }
-
+}
 };
+
+
+//source-map in devtool is used to for producton and it takes a lot of time for build 
+////is used for   build:prod": "webpack -p --env production",
+//whenever we run yarn run build:prod is used for production and we can get env varibale as production 
+//we can optimise the webpack file for production and we are reducing unwanted code and we are making it optimised
+//
+//for production we are using one source map and for dev we are using different source map
 
 //this is the config file where we will give entry for file
 //the output file
@@ -45,3 +76,5 @@ module.exports = {
 //dev server can help you to run and for that we have to run yarn run dev-server command
 
 //w used normalize.css to support styles in all browsers and we installed normalize.css for that
+//we have used extract-text-webpack-plugin for to generate a single styles.css
+//we have to use in production and we have used css-loaders and sass-loaders 
